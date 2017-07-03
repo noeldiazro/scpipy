bceff9046b64a9efcdc47f364fc3b6a4e354c262
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2017-07-03 17:13:38 +0000

    Initial commit

---
+++ scpipy.py
@@ -0,0 +1,34 @@
+from socket import socket
+
+class TcpIpAddress(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+class TcpIpLink(object):
+    def __init__(self, address, timeout=None, alt_socket=None):
+        self.address = address
+        self.timeout = timeout
+        self._socket = alt_socket or socket()
+
+        if timeout is not None:
+            self._socket.settimeout(timeout)
+        
+    def open(self):
+        self._socket.connect((self.address.host, self.address.port))
+
+    def close(self):
+        self._socket.close()
+
+    def read(self, number_of_bytes):
+        return self._socket.recv(number_of_bytes)
+
+    def write(self, request):
+        return self._socket.send(request)
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
---
+++ test_scpipy.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+from scpipy import TcpIpLink, TcpIpAddress
+
+class TcpIpLinkTest(TestCase):
+    host = 'rp-f0060c.local'
+    port = 5000
+    timeout = 1
+    
+    def setUp(self):
+        self.link = TcpIpLink(TcpIpAddress(self.host, self.port), self.timeout, alt_socket=TestSocket('abcde'))
+        self.link.open()
+        
+    def test_create_ethernet_link(self):
+        self.assertEqual(self.host, self.link.address.host)
+        self.assertEqual(self.port, self.link.address.port)
+        self.assertEqual(self.timeout, self.link.timeout)    
+
+    def test_read_ethernet_link(self):
+        self.link.open()
+        response = self.link.read(4096)
+        self.assertEqual('abcde', response)
+
+    def test_write_ethernet_link(self):
+        number_of_bytes = self.link.write(request='12345')
+        self.assertEqual(5, number_of_bytes)
+        
+    def tearDown(self):
+        self.link.close()
+        
+
+class TestSocket(object):
+    def __init__(self, buffer=''):
+        self._buffer = buffer
+    
+    def settimeout(self, timeout):
+        pass
+
+    def connect(self, address):
+        pass
+
+    def close(self):
+        pass
+
+    def recv(self, number_of_bytes):
+        return self._buffer
+
+    def send(self, request):
+        self._buffer = request
+        return len(request)
+    
