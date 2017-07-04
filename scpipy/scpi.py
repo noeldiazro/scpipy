8b95f2e807ccfa1dc084660bdcdd4f2b1d43e4f5
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2017-07-04 09:11:57 +0000

    Añadida clase ScpiConnection

---
+++ scpipy/scpi.py
@@ -0,0 +1,14 @@
+class ScpiConnection(object):
+    delimiter = '\r\n'
+    
+    def __init__(self, link):
+        self._link = link
+
+    def open(self):
+        self._link.open()
+
+    def close(self):
+        self._link.close()
+
+    def write(self, message):
+        return self._link.write(message + self.delimiter) - len(self.delimiter)
---
+++ test/test_scpi.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+from mock import Mock
+from scpipy import ScpiConnection
+from scpipy.links import TcpIpLink
+
+class ScpiConnectionTest(TestCase):
+    def test_create_scpi_connection(self):
+        test_link = Mock(TcpIpLink)
+        connection = ScpiConnection(test_link)
+
+    def test_open_scpi_connection(self):
+        test_link = Mock(TcpIpLink)
+        connection = ScpiConnection(test_link)
+        connection.open()
+
+    def test_close_spi_connection(self):
+        test_link = Mock(TcpIpLink)
+        connection = ScpiConnection(test_link)
+        connection.close()
+
+    def test_write_spi_connection(self):
+        message = 'DIG:PIN LED0,1'
+        test_link = Mock(TcpIpLink)
+        test_link.write.return_value = len(message) + 2
+        connection = ScpiConnection(test_link)
+
+        number_of_bytes = connection.write(message)
+
+        self.assertEqual(len(message), number_of_bytes)
