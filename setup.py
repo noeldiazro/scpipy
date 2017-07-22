ed0ec1cb2b18e1d82bcb6f1d39994126763ec7ae
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2017-07-22 10:54:39 +0000

    Añadido script de empaquetado y distribución

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='scpipy',
+    version='0.1',
+    description='Control SCPI devices',
+    author='Noel Díaz Rodríguez',
+    author_email='noeldiazro@gmail.com',
+    packages=['scpipy']
+)
